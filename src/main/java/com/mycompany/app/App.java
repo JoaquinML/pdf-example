cc6c2d83d3e70aa6f8531bba21233b5b94641deb
JoaquinML <joaquinmartinez2509@gmail.com>
2019-11-09 17:19:53 -0600

    first working version

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,37 @@
 package com.mycompany.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfWriter;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws DocumentException, FileNotFoundException, IOException
     {
-        System.out.println( "Hello World!" );
+        File newPdf = File.createTempFile("test", ".pdf");
+        FileOutputStream os = new FileOutputStream(newPdf);
+
+        Document document = new Document();
+        PdfWriter.getInstance(document, os);
+ 
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk("Hello World", font);
+        
+        document.add(chunk);
+        document.close();
     }
 }
