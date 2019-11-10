ec93051045d68c8f34c8c0f253285e244fd76e7b
JoaquinML <joaquinmartinez2509@gmail.com>
2019-11-09 19:45:20 -0600

    modify to test the library

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,12 @@
 package com.mycompany.app;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
@@ -28,10 +31,20 @@ public class App
         PdfWriter.getInstance(document, os);
  
         document.open();
-        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-        Chunk chunk = new Chunk("Hello World", font);
-        
-        document.add(chunk);
+        Font font = FontFactory.getFont(FontFactory.COURIER, 20, BaseColor.BLACK);
+        Chunk title = new Chunk("Print your format", font);
+        document.add(title);
         document.close();
+
+        
+        for(byte b : Files.readAllBytes(Paths.get(newPdf.getAbsolutePath()))){
+            System.out.print(b);
+        }
+
+        File newf = new File("test.pdf");
+        FileOutputStream os2 = new FileOutputStream(newf);
+        os2.write(Files.readAllBytes(Paths.get(newPdf.getAbsolutePath())));
+        os2.close();
+        
     }
 }
